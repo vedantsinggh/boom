68ba960338a249fa248d629c3bfcb78f181a43b3
vedantsinggh <vedantsinggh@gmail.com>
2024-10-05 16:34:00 +0530

    added front line

--- main.c
+++ main.c
@@ -8,28 +8,22 @@
 #define ROTATION_SPEED 0.1f
 #define PLAYER_RADIUS 10
 #define ANGLE_OF_VIEW 45
+#define LINE_OF_VIEW  100
 
 int maps[10][10] =  {
 	{1,1,1,1,1,1,1,1,1,1},
 	{1,0,0,0,0,0,0,0,0,1},
-	{1,0,0,0,0,0,0,0,0,1},
-	{1,0,0,0,0,0,0,0,0,1},
-	{1,0,0,0,0,0,0,0,0,1},
-	{1,0,0,0,0,0,0,0,0,1},
-	{1,0,0,0,0,0,0,1,1,1},
+	{1,0,0,1,0,0,0,0,0,1},
+	{1,0,0,1,0,0,0,0,0,1},
+	{1,0,0,1,0,0,0,0,0,1},
+	{1,0,0,1,0,0,0,0,0,1},
+	{1,0,0,1,0,0,0,1,1,1},
 	{1,0,0,0,0,0,0,1,1,1},
 	{1,0,0,0,0,0,0,1,1,1},
 	{1,1,1,1,1,1,1,1,1,1},
 };
 
 void Grid(){
-	for (int i=0; i <= WIDTH; i+= WIDTH/GRID_SIZE){
-		 DrawLine(i, 0,i,HEIGHT, BLACK );
-	}
-	for (int i=0; i <= HEIGHT; i+= HEIGHT/GRID_SIZE){
-		 DrawLine(0, i,WIDTH,i, BLACK );
-	}
-	
 	for(int i=0; i < GRID_SIZE; i++){
 		for(int j=0; j < GRID_SIZE; j++){
 			if (maps[i][j] != 0){
@@ -37,11 +31,26 @@ void Grid(){
 			}
 		}
 	}
+	for (int i=0; i <= WIDTH; i+= WIDTH/GRID_SIZE){
+		 DrawLine(i, 0,i,HEIGHT, BLACK );
+	}
+	for (int i=0; i <= HEIGHT; i+= HEIGHT/GRID_SIZE){
+		 DrawLine(0, i,WIDTH,i, BLACK );
+	}
 }
 
 void DrawPlayer(Vector3 pos){
 	DrawCircle(pos.x,pos.y, PLAYER_RADIUS, RED);
 	DrawCircleSector((Vector2){pos.x, pos.y},PLAYER_RADIUS, pos.z - ANGLE_OF_VIEW/2,pos.z  + ANGLE_OF_VIEW/2, 5, YELLOW);
+	
+
+	//here we draw player FOV
+	float theta = pos.z * (PI / 180.0f);
+	float front_x = pos.x + LINE_OF_VIEW * cosf(theta);
+	float front_y = pos.y + LINE_OF_VIEW * sinf(theta);
+	DrawCircle(front_x, front_y, 2, GREEN);
+
+	DrawLine(pos.x, pos.y, front_x, front_y, GREEN);
 }
 
 void Update(Vector3* player){
